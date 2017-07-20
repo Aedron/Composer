d054a892348a5cf20764379b07d799de5b775162
HuQingyang <hqy841440305@gmail.com>
2017-07-21 00:29:13 +0800

    Building parser

--- src/lib/parser.js
+++ src/lib/parser.js
@@ -1,28 +1,27 @@
 
 export default function (content) {
+    if (content.innerHTML === '') return (content.innerHTML = '<div><br/></div>')
     const data = Array.from(content.children)
     data.forEach(parserLine)
-    return content;
+    return content
 }
 
 
 
 function parserLine(line) {
-    console.log(line)
+    parserH(line)
 }
 
 
 function parserH(line) {
-    const slice = line.innerHTML.split(' ');
-    const tag = slice[0];
+    const slice = line.innerHTML.split(' ')
+    if (slice.length < 2) return
+    const tag = slice.shift()
+    const content = slice.join(' ')
 
     if (['#', '##', '###', '####', '#####'].includes(tag)) {
-        line.className = `h${}`
+        // line.className = `tag h${tag.length}`
+        line.innerHTML = `<span class="tag h${tag.length}">${tag}</span><span class="h${tag.length}">${content}</span>`
+        console.log(line)
     }
-
-    slice.shift();
-    const content = slice.join(' ');
-
-    const num = tag.length;
-    return `<h${num}>${content}</h${num}>`
 }
