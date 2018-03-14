e735f3e1ad8eba436436fdd34822b6c5e9028217
HuQingyang <hqy841440305@gmail.com>
2018-03-14 23:59:10 +0800

    Init

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,21 @@
 
-import injectProps from './prototype/index.ts'
-import parse from './parser/index.ts'
-
-
 interface Config {
-    el: HTMLElement
+
 }
 
-export default function (el: HTMLElement, config?: Config) {
-    const content = document.createElement('div')
-    content.innerHTML = 'Edit Here...'
-    el.className = el.className ? `${el.className} composeContainer` : 'composeContainer'
-    el.contentEditable = 'true'
-    el.appendChild(content)
-    content.focus()
-    window.addEventListener('input', handleKeyUp.bind(this))
+const defaultConfig = {
 
-    this.dom = el
-    this.lastCurPos = null;
-    injectProps.bind(this)()
-}
+};
 
+class Editor {
+    static create = (el: HTMLElement, config?: Config) => {
+        return new Editor(el, config);
+    };
 
-function handleKeyUp() {
-    if (checkEmpty(this.dom)) return
-    else {
-        const pos = this.curPos;
-        parse(this.focusDom)
-        this.curPos = pos;
+    config: Config;
+
+    constructor(el: HTMLElement, config: Config) {
+
     }
-}
 
-function checkEmpty(dom: HTMLElement) {
-    Array.from(dom.children).forEach(d => {
-        if (d.innerHTML === '' || d.innerHTML === '')
-            dom.removeChild(d)
-    })
-    if (dom.innerHTML === '' || dom.innerHTML === '<br>') {
-        dom.innerHTML = '<br>'
-        dom.focus()
-    }
 }
